9c4666a92600245cdd542f53a48a5dc0bcd0a507
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-05-12 23:56:20 +0800

    refactor: update DATABASE_URL handling and simplify Docker Compose configuration

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres:5432/fastapi_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    url=DATABASE_URL,
+)
 SessionLocal = sessionmaker(bind=engine)
