b33d5dadc4b3e6877c29aa3d7d2fd60e6d7b3d46
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-05-14 01:35:36 +0800

    refactor: simplify database connection setup by using DATABASE_URL environment variable

--- database.py
+++ database.py
@@ -3,12 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_USER = os.getenv("POSTGRES_USER", "postgres")
-DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-DATABASE_DB = os.getenv("POSTGRES_DB", "fastapi_db")
-DATABASE_HOST = os.getenv("POSTGRES_HOST", "localhost")
-DATABASE_PORT = os.getenv("POSTGRES_PORT", "5432")
-DATABASE_URL = f"postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
