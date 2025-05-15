a13a453f48a7ea736a264f64410ffe0e4c81ea61
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-05-16 00:17:31 +0800

    feat: add sslmode

--- database.py
+++ database.py
@@ -5,5 +5,5 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(bind=engine)
